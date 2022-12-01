c4ebefe5c64ffacbe5c5221a5e013728baea53e8
Arthur Ming <guobin.ming@pwc.com>
2022-12-01 13:21:43 +0800

    chore: init commit

---
+++ user/src/main/java/org/daming/hoteler/user/HotelerUserApplication.java
@@ -0,0 +1,16 @@
+package org.daming.hoteler.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gming001
+ * @version 2022-12-01 11:47
+ */
+@SpringBootApplication
+public class HotelerUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelerUserApplication.class, args);
+    }
+}
