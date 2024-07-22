9b159b7f5292b3e5d047d93fd28a71f6afb95f4d
Arthur Ming <guobin.ming@pwc.com>
2024-07-22 14:13:54 +0800

    chore(docker): run on openjdk 22

---
+++ build-docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 定义 Maven 容器名称
+mavenContainerName="maven-build"
+
+# 构建 Maven 容器
+docker build -t $mavenContainerName -f Dockerfile .
+
+# 定义 targets
+targets=("auth" "workflow" "orchestration")
+
+# 循环构建每个 target 镜像
+for target in "${targets[@]}"; do
+    targetImageName="damingerdai/$target"
+
+    # 构建 target 镜像
+    docker build -t $targetImageName -f Dockerfile --build-arg target=$target .
+done
