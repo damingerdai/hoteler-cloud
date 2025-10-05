769163f2c0634e6db5d7536cb5fc76295709d804
damingerdai <mingguobin@live.com>
2025-10-05 08:25:10 +0800

    chore: add build podman scripts

---
+++ build-podman.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+sha=$(git rev-parse --short HEAD)
+author=$(git log -1 --pretty=format:'%an')  # 获取提交者的用户名
+
+# 定义 Maven 容器名称
+mavenContainerName="maven-build"
+
+# 构建 Maven 容器
+podman build -t $mavenContainerName -f Dockerfile . || { echo "Maven 容器构建失败"; exit 1; }
+
+# 定义 targets
+targets="auth workflow orchestration"
+
+# 循环构建每个 target 镜像
+for target in $targets; do
+  targetImageName="damingerdai/$target"
+
+  # 构建 target 镜像
+  podman build -t $targetImageName:${sha} -f Dockerfile --build-arg target=$target . || { echo "目标镜像 $targetImageName:${sha} 构建失败"; exit 1; }
+done
